05c15bdc98675b7ac1ee6bcd2434105912dd7c3e
lifaqiang15 <lifaqiang15@163.com>
2025-08-15 00:41:54 +0800

    feat: 添加登录组件

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,19 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import IndexView from '@/views/IndexView.vue'
+import LoginView from '@/views/LoginView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
       name: 'index',
       component: IndexView,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
     },
   ],
 })
